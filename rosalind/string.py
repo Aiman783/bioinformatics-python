a973570c181ab80ff419ba00dd09c68ddd192bc2
Aiman783 <aimanahmad665@gmail.com>
2026-09-01 17:06:57 +0530

    Create string.py
    Add string slicing solution

---
+++ rosalind/string.py
@@ -0,0 +1,10 @@
+s = "PSQ435Do4MR2TgUP3EwSeyf0bNcHYIHj7DyVEqZARqJyRygb3UUxQ3cJtuehyVA5Mj8h4AzXqz7STnAUw7pPpnA1KPMTadaridam5opW9UFAOrK80QtpEYsGwqFctKZ1pmkLLSve4ipchncvIp5jguttiferov3OYVjNLaXQRkCz93gGecCgY75peerPj."
+a = 91
+b = 98
+c = 148
+d = 155
+
+first = s[a:b+1]
+second = s[c:d+1]
+
+print(first, second)
